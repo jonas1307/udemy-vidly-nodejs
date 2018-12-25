57b0496fd348c5f4f8950f07e3e4605b6262de29
Jonas Lima <jonas1307@live.com>
2018-12-25 14:16:50 -0200

    Adding customers API

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 
 mongoose.connect('mongodb://localhost/vidly')
 .then(() => console.log('MongoDB connected.'))
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://localhost/vidly')
 app.use(express.json());
 
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
