4350d8ae2039f2c1eb422d9d80b6c904df72fe39
Jonas Lima <jonas1307@live.com>
2019-01-15 15:54:27 -0200

    Implementing isAdmin authorization with middleware

--- routes/genres.js
+++ routes/genres.js
@@ -2,6 +2,7 @@ const { Genre, validate } = require('../models/genre');
 const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth');
+const admin = require('../middlewares/admin');
 
 router.get('/', async (req, res) => {
     const genres = await Genre.find().sort('name');
@@ -32,7 +33,7 @@ router.put('/:id', auth, async (req, res) => {
     res.send(genre);
 });
 
-router.delete('/:id', auth, async (req, res) => {
+router.delete('/:id', [auth, admin], async (req, res) => {
     const genre = await Genre.findByIdAndRemove(req.params.id);
     if (!genre) return res.status(404).send('The genre with given ID was not found.');
     res.send(genre);
