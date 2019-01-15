9b113236bd491d52c937beea3238b39a29dc495c
Jonas Lima <jonas1307@live.com>
2019-01-15 14:17:36 -0200

    Adding and implementing auth middleware

--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,7 @@
 const { Customer, validate } = require('../models/customer');
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/auth');
 
 router.get('/', async (req, res) => {
     const customers = await Customer.find();
@@ -13,7 +14,7 @@ router.get('/:id', async (req, res) => {
     res.send(customer);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -26,7 +27,7 @@ router.post('/', async (req, res) => {
     res.send(customer);
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', auth, async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -39,7 +40,7 @@ router.put('/:id', async (req, res) => {
     res.send(customer);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
     const customer = await Customer.findByIdAndRemove(req.params.id);
     if (!customer) return res.status(404).send('The customer with the given ID was not found.');
     res.send(customer);
--- routes/genres.js
+++ routes/genres.js
@@ -1,6 +1,7 @@
 const { Genre, validate } = require('../models/genre');
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/auth');
 
 router.get('/', async (req, res) => {
     const genres = await Genre.find().sort('name');
@@ -13,7 +14,7 @@ router.get('/:id', async (req, res) => {
     res.send(genre);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -22,7 +23,7 @@ router.post('/', async (req, res) => {
     res.send(genre);
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', auth, async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -31,7 +32,7 @@ router.put('/:id', async (req, res) => {
     res.send(genre);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
     const genre = await Genre.findByIdAndRemove(req.params.id);
     if (!genre) return res.status(404).send('The genre with given ID was not found.');
     res.send(genre);
