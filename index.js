a2b1d2b9020d1025d5ace1a587b688ee5471d3a7
Jonas Lima <jonas1307@live.com>
2019-01-19 19:47:50 -0200

    Using winston to log uncaughtExceptions and unhandledRejections

--- index.js
+++ index.js
@@ -14,15 +14,12 @@ const error = require('./middlewares/error');
 const winston = require('winston');
 
 winston.add(winston.transports.File, {filename: 'exceptions.log'});
-
-process.on('uncaughtException', (ex) => {
-    winston.error(ex.message, ex);
-    process.exit(1);
-});
-
+winston.handleExceptions(
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+);
+  
 process.on('unhandledRejection', (ex) => {
-    winston.error(ex.message, ex);
-    process.exit(1);
+    throw ex;
 });
 
 if (!config.get('jwtPrivateKey')){
